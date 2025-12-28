1da7f8004d7ffee5d2574e58d21a65f3ca6f1d43
beasvarela <beatriz.varela9@gmail.com>
2025-12-28 19:49:48 +0000

    port changes

--- app.py
+++ app.py
@@ -606,5 +606,8 @@ def update_plot(state, income, flights, months, distance, recency):
 
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=False)
+    import os
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port)
+
 
